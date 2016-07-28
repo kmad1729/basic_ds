20e472c07d0348ba642dbc53bab1e1ae6526253a
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-07-27 21:42:48 -0700

    adding some more functions

--- clrs/ch6_heapsort/py_heapsort/heap.py
+++ clrs/ch6_heapsort/py_heapsort/heap.py
@@ -18,4 +18,15 @@ def max_heapify(lst, ind, heap_size):
         lst[ind], lst[largest] = lst[largest], lst[ind]
         max_heapify(lst, largest, heap_size)
 
+def build_max_heap(lst):
+    heap_size = len(lst)
+    for i in range( (heap_size - 1) // 2, -1, -1):
+        max_heapify(lst, i, heap_size)
 
+def heap_sort(lst):
+    build_max_heap(lst)
+    heap_size = len(lst)
+    for i in range(heap_size - 1, 0, -1):
+        lst[0], lst[i] = lst[i], lst[0]
+        heap_size -= 1
+        max_heapify(lst, 0, heap_size)
--- clrs/ch6_heapsort/py_heapsort/test_heap.py
+++ clrs/ch6_heapsort/py_heapsort/test_heap.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 'tests for heap.py function'
 
+from random import randint
 import unittest
-from heap import max_heapify
+
+from heap import max_heapify, build_max_heap, heap_sort
 
 class TestHeaps(unittest.TestCase):
 
+    def setUp(self):
+        self.rand_list = [randint(0, 50) for i in range(25)]
+
     def test_max_heapify_basic(self):
         lst = [2, 5, 9, 4, 3, 4, 6, 2, 1, 0]
-        print("before max heapify lst = {}".format(lst))
         max_heapify(lst, 0, len(lst))
-        print("after  max_heapify(lst, 0, len(lst)). lst = {}".format(lst))
         exp_out = [9, 5, 6, 4, 3, 4, 2, 2, 1, 0]
         self.assertEqual(exp_out, lst)
 
@@ -20,6 +23,30 @@ class TestHeaps(unittest.TestCase):
         exp_out = [12, 4, 9, 2, 3, 4, 6, 2, 1, 0]
         self.assertEqual(exp_out, lst)
 
+    def test_build_max_heap(self):
+        lst = [2, 5, 8, 7, 4, 6, 9, 14, 6, 27]
+        build_max_heap(lst)
+        exp_out = [27, 14, 9, 7, 5, 6, 8, 2, 6, 4]
+        self.assertEqual(exp_out, lst)
+
+    def test_heap_sort(self):
+        rand_list_copy = self.rand_list[:]
+        rand_list_copy.sort()
+        print("rand list = {self.rand_list}".format(self = self))
+        heap_sort(self.rand_list)
+        print("rand list sorted = {self.rand_list}".format(self = self))
+        print('#' * 20)
+        self.assertEqual(rand_list_copy, self.rand_list)
+
+    def test_heap_sort_single_elem(self):
+        lst = [5]
+        heap_sort(lst)
+        self.assertEqual([5], lst)
+
+    def test_heap_sort_0_elem(self):
+        lst = []
+        heap_sort(lst)
+        self.assertEqual([], lst)
 
 
 if __name__ == '__main__':
