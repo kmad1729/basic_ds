647fe8014f7ea329924fdd53f471ee9c6b404cb9
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-07-27 22:19:10 -0700

    adding more priority queue related methods

--- clrs/ch6_heapsort/py_heapsort/heap.py
+++ clrs/ch6_heapsort/py_heapsort/heap.py
@@ -30,3 +30,33 @@ def heap_sort(lst):
         lst[0], lst[i] = lst[i], lst[0]
         heap_size -= 1
         max_heapify(lst, 0, heap_size)
+
+#priority queue related methods
+def heap_max(lst):
+    return lst[0]
+
+def heap_extract_max(lst):
+    assert(len(lst)) > 0
+    lst[0], lst[-1] = lst[-1], lst[0]
+    result = lst.pop()
+    max_heapify(lst, 0, len(lst))
+    return result
+
+def _parent(i):
+    return (i - 1) >> 1
+
+def heap_increase_key(lst, ind, key):
+    assert 0 <= ind < len(lst)
+    if key <= lst[ind]:
+        raise Exception(
+                "current key value at {i} is already {l_i} which is >= {key}".format(
+                    i = ind, l_i = lst[ind], key = key))
+    lst[ind] = key
+    while ind > 0 and lst[_parent(ind)] < lst[ind]:
+        lst[_parent(ind)], lst[ind] = lst[ind], lst[_parent(ind)]
+        ind = _parent(ind)
+
+
+def max_heap_insert(lst, key, lst_range_min):
+    lst.append(lst_range_min)
+    heap_increase_key(lst, len(lst) - 1, key)
--- clrs/ch6_heapsort/py_heapsort/test_heap.py
+++ clrs/ch6_heapsort/py_heapsort/test_heap.py
@@ -4,7 +4,9 @@
 from random import randint
 import unittest
 
-from heap import max_heapify, build_max_heap, heap_sort
+from heap import max_heapify, build_max_heap, heap_sort, \
+    heap_max, heap_extract_max, _parent, heap_increase_key, \
+    max_heap_insert
 
 class TestHeaps(unittest.TestCase):
 
@@ -47,6 +49,56 @@ class TestHeaps(unittest.TestCase):
         lst = []
         heap_sort(lst)
         self.assertEqual([], lst)
+
+    def test_heap_max(self):
+        lst = list(range(100, 0, -3))
+        self.assertEqual(100, heap_max(lst))
+
+    def test_heap_extract_max(self):
+        lst = list(range(100, 0, -3))
+        expected_len = len(lst)
+        for i in range(100, 0, -3):
+            exp_max = i
+            self.assertEqual(exp_max, heap_extract_max(lst))
+            expected_len -= 1
+            self.assertEqual(expected_len, len(lst))
+
+    def test__parent(self):
+        ind_parent_map = {2 : 0, 1: 0, 5: 2, 8 : 3, 7 : 3, 3 : 1}
+        for ind, parent in ind_parent_map.items():
+            self.assertEqual(parent, _parent(ind))
+
+    def test_heap_increase_key(self):
+        lst = list(range(10))
+        print("lst = {}".format(lst))
+        build_max_heap(lst)
+        print("lst after heap = {}".format(lst))
+        print("increasing last elem key to 100")
+        heap_increase_key(lst, len(lst) - 1, 100)
+        print("lst after heap_increase_key = {}".format(lst))
+        print('#' * 20)
+        self.assertEqual(100, heap_max(lst))
+
+    def test_max_heap_insert(self):
+        max_heap = [27, 14, 9, 7, 5, 6, 8, 2, 6, 4]
+        initial_len = len(max_heap)
+        print("max_heap = {}".format(max_heap))
+        i = 20
+
+        print("inserting {i} to the max_heap".format(i = i))
+        max_heap_insert(max_heap, i, -1)
+        print("new_heap = {}".format(max_heap))
+        self.assertEqual(len(max_heap), initial_len + 1)
+        exp_new_heap = [27, 20, 9, 7, 14, 6, 8, 2, 6, 4, 5]
+        self.assertEqual(exp_new_heap, max_heap)
+
+        print("inserting {i} to the max_heap".format(i = i))
+        max_heap_insert(max_heap, i, -1)
+        print("new_heap = {}".format(max_heap))
+        print("#" * 20)
+        exp_new_heap = [27, 20, 20, 7, 14, 9, 8, 2, 6, 4, 5, 6]
+        self.assertEqual(exp_new_heap, max_heap)
+
 
 
 if __name__ == '__main__':
