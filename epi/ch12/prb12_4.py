71ba09477f30c679075fd9bb767b79ba42588987
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-08-11 22:18:18 -0700

    adding solution to prb 12 4

---
+++ epi/ch12/prb12_4.py
@@ -0,0 +1,34 @@
+'compute the math.floor of square too of n'
+
+import unittest
+import math
+import sys
+
+def get_sqrt_math_floor(n):
+    if n < 2:
+        return n
+    l, u = 0, n
+    while l <= u:
+        m = l + (u - l) // 2
+        m_sqrd = m * m
+        if m_sqrd <= n:
+            l = m + 1
+        else:
+            u = m - 1
+    return l - 1
+
+
+class Test_GetSqrtMathFloor(unittest.TestCase):
+
+    def test_basic_functionality(self):
+
+        for k in range(1000, -1, -1):
+            result = get_sqrt_math_floor(k)
+            expected = math.floor(math.sqrt(k))
+            #print("result = {} math.sqrt = {}".format(result, expected))
+            self.assertEqual(result, expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
