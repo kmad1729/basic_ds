bd4cdfee536f72715d135ed3a470b3f731ac6bc5
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-08-18 23:40:11 -0700

    adding tests for quicksort implementation

--- clrs/ch7_quicksort/test_quicksort_impl.py
+++ clrs/ch7_quicksort/test_quicksort_impl.py
@@ -2,6 +2,7 @@
 
 from quicksort_impl import quicksort, partition
 
+from random import randint
 import unittest
 
 class Test_Quicksort_impl(unittest.TestCase):
@@ -19,6 +20,29 @@ class Test_Quicksort_impl(unittest.TestCase):
 
         lst2 = [5] * 5
         self.assertEqual(0, partition(lst2, 0, len(lst2) - 1))
+
+    def test_quicksort(self):
+        lst1 = [2, 5, 4, 7, 3, 1, 6]
+        sorted_list = sorted(lst1)
+        quicksort(lst1, 0, len(lst1) - 1)
+        self.assertEqual(sorted_list, lst1)
+
+        lst2 = [5] * 10
+        quicksort(lst2, 0, len(lst2) - 1)
+        self.assertEqual(lst2, [5] * 10)
+
+        lst3 = []
+        quicksort(lst3, 0, len(lst3) - 1)
+        self.assertEqual(lst3, [])
+
+        size = 100000
+        rand_arr = [randint(0, 200) for i in range(size)]
+        sorted_rand_arr = sorted(rand_arr)
+
+        quicksort(rand_arr, 0, size - 1)
+        self.assertEqual(sorted_rand_arr, rand_arr)
+
+
 
 
 if __name__ == '__main__':
