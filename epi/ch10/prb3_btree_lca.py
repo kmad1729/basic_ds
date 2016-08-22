bffe14174c593b856129d7731362fb57e27f0117
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-08-22 08:49:49 -0700

    Adding ch10 problem solutions

---
+++ epi/ch10/prb3_btree_lca.py
@@ -0,0 +1,9 @@
+'get the least common ancestor of two nodes in a generic binary tree'
+
+from binary_tree import BinaryTree
+
+'''Binary tree represented as
+{'data': 1, 'left': {'data':-1, 'left': None, 'right': None}, 'right':None}
+'''
+
+def get_
