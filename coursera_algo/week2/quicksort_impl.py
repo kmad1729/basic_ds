7f28fc6eabc76eefe163766bfa3356541893266d
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-08-21 19:19:45 -0700

    adding test cases from instructor

--- coursera_algo/week2/quicksort_impl.py
+++ coursera_algo/week2/quicksort_impl.py
@@ -1,26 +1,26 @@
 'quicksort implementation from the class.'
 
-def quicksort(A, p, r, default_pivot_ind = (lambda arr, b, e: b)):
+def quicksort(A, p, r, get_default_pivot_fun = (lambda arr, b, e: b)):
     '''implementation of quicksort. Sort an array A[p...r] p and r inclusive.
-    Pivot is always the 1st elem of the array.  Returns the number of comparisons.
-    get_default_pivot_fun functionis returns the default pivot index for array A from [p..r]
+    If get_default_pivot_fun is not given, Pivot is always the 1st elem of the array.  
+    Returns the number of comparisons.
+    get_default_pivot_fun functionreturns the default pivot index for array A from [p..r]
     '''
 
     if p >= r:
         return 0
 
-
-    pivot_ind = partition(A, p, r, default_pivot_ind)
-    left_comparisons = quicksort(A, p, pivot_ind - 1, default_pivot_ind)
-    right_comparisons = quicksort(A, pivot_ind + 1, r, default_pivot_ind)
+    pivot_ind = partition(A, p, r, get_default_pivot_fun)
+    left_comparisons = quicksort(A, p, pivot_ind - 1, get_default_pivot_fun)
+    right_comparisons = quicksort(A, pivot_ind + 1, r, get_default_pivot_fun)
     return (r - p + left_comparisons + right_comparisons)
 
-def partition(A, l, r, default_pivot_ind):
+def partition(A, l, r, get_default_pivot_fun):
     '''Partitions array A with pivot at index 0. Returns the 0-based index of pivot in a 
     sorted array.
     '''
 
-    pivot_ind = default_pivot_ind(A, l, r)
+    pivot_ind = get_default_pivot_fun(A, l, r)
     A[l], A[pivot_ind] = A[pivot_ind], A[l]
     pivot = A[l]
     i = l + 1
--- coursera_algo/week2/test_quicksort_impl.py
+++ coursera_algo/week2/test_quicksort_impl.py
@@ -5,13 +5,31 @@ from random import randint
 
 from quicksort_impl import quicksort, partition
 
+def read_inp_file(f_name):
+    result = []
+    with open(f_name, 'r') as f:
+        for l in f:
+            if l.strip() != '':
+                result.append(int(l))
+    return result
+
+def first_elem_piv(A, p, r):
+    return p
+def last_elem_piv(A, p, r):
+    return r
+
+def median_of_3_piv(A, p , r):
+    first_elem = (A[p], p)
+    last_elem = (A[r], r)
+    mid_elem_ind = p + (r - p) // 2
+    mid_elem = (A[mid_elem_ind], mid_elem_ind)
+    return sorted([first_elem, last_elem, mid_elem])[1][1]
+
 class TestQuickSort(unittest.TestCase):
     def test_partition(self):
-        def default_pivot_ind(A, p, r):
-            return p
         arr = [4, 5, 2, 1, 7, 6, 3]
-        self.assertEqual(3, partition(arr, 0, len(arr) - 1, default_pivot_ind))
-        self.assertEqual(2, partition(arr, 0, len(arr) - 1, default_pivot_ind))
+        self.assertEqual(3, partition(arr, 0, len(arr) - 1, first_elem_piv))
+        self.assertEqual(2, partition(arr, 0, len(arr) - 1, first_elem_piv))
 
     def test_quicksort(self):
         arr = [4, 5, 2, 1, 7, 6, 3]
@@ -36,9 +54,24 @@ class TestQuickSort(unittest.TestCase):
         comps1 = quicksort(arr, 0, len(arr) - 1, default_pivot_ind)
         print("number of comparision for arr = {}".format(comps1))
         self.assertEqual(arr, list(range(1, 600)))
-        #comparisions of sorted array should be (n - 1)(n) // 2
-        #self.assertEqual(17997000, comps1)
-        #self.assertEqual(17997000, comps2)
+
+    def test_instructor_test_cases(self):
+        d_10_elem = read_inp_file('inp1.txt')
+        self.assertEqual(25, quicksort(d_10_elem[:], 0, len(d_10_elem) - 1, first_elem_piv))
+        self.assertEqual(29, quicksort(d_10_elem[:], 0, len(d_10_elem) - 1, last_elem_piv))
+        self.assertEqual(21, quicksort(d_10_elem[:], 0, len(d_10_elem) - 1, median_of_3_piv))
+
+        d_100_elem = read_inp_file('100.txt')
+        self.assertEqual(615, quicksort(d_100_elem[:], 0, len(d_100_elem) - 1, first_elem_piv))
+        self.assertEqual(587, quicksort(d_100_elem[:], 0, len(d_100_elem) - 1, last_elem_piv))
+        self.assertEqual(518, quicksort(d_100_elem[:], 0, len(d_100_elem) - 1, median_of_3_piv))
+
+        d_1000_elem = read_inp_file('1000.txt')
+        self.assertEqual(10297, quicksort(d_1000_elem[:], 0, len(d_1000_elem) - 1, first_elem_piv))
+        self.assertEqual(10184, quicksort(d_1000_elem[:], 0, len(d_1000_elem) - 1, last_elem_piv))
+        self.assertEqual(8921, quicksort(d_1000_elem[:], 0, len(d_1000_elem) - 1, median_of_3_piv))
+
+
 
 
 if __name__ == '__main__':
