462fbb031ab315b9174c228f5031c10db0515f49
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-08-30 23:45:24 -0700

    adding randomized select algorithm

--- clrs/ch9_median_order_statistic/prb_9_2_0.py
+++ clrs/ch9_median_order_statistic/prb_9_2_0.py
@@ -17,3 +17,17 @@ def randomized_partition(A, start_ind, end_ind):
     A[start_ind], A[i - 1] = A[i - 1], A[start_ind]
     return i - 1
 
+
+def random_select(A, start_ind, end_ind, i_order_to_find):
+    'find the ith order statistic from array A'
+
+    j = randomized_partition(A, start_ind, end_ind)
+    k = j - start_ind
+
+    if k + 1 == i_order_to_find:
+        return A[j]
+    elif k + 1 > i_order_to_find:
+        return random_select(A, start_ind, j - 1, i_order_to_find)
+    else:
+        return random_select(A, j + 1, end_ind, i_order_to_find -k -1)
+
--- clrs/ch9_median_order_statistic/test_prb920.py
+++ clrs/ch9_median_order_statistic/test_prb920.py
@@ -1,14 +1,14 @@
 import unittest
 from random import randint
 
-from prb_9_2_0 import randomized_partition
+from prb_9_2_0 import randomized_partition, random_select
 
 
 class Test_randomized_partition(unittest.TestCase):
 
     def test_basic_functionality(self):
         sz = 100
-        A = [randint(-5, 20) for i in range(sz)]
+        A = [randint(-20, 20) for i in range(sz)]
         part_ind = randomized_partition(A, 0, sz - 1)
 
         for i in range(part_ind):
@@ -16,6 +16,22 @@ class Test_randomized_partition(unittest.TestCase):
 
         for i in range(part_ind + 1, sz):
             self.assertTrue(A[i] >= A[part_ind])
+
+class Test_randomize_select(unittest.TestCase):
+    def test_basic_functionality(self):
+        A = [7, 1, 3, 4, 2, 8, 5, 6]
+
+        i = 6
+        self.assertEqual(i, random_select(A, 0, len(A) - 1, i))
+        print("after finding {}th smallest element A -->".format(i))
+        print(A)
+
+        i = 3
+        self.assertEqual(i, random_select(A, 0, len(A) - 1, i))
+        print("after finding {}rd smallest element A -->".format(i))
+        print(A)
+
+
 
 if __name__ == '__main__':
     unittest.main()
