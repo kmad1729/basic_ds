51a54008854dcbeb3bbf0b65fa2bb16b10d02630
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-09-02 23:35:52 -0700

    adding function signature for node to leaf sum

--- epi/ch10/prb5_btree_path_leaf_sum.py
+++ epi/ch10/prb5_btree_path_leaf_sum.py
@@ -1,3 +1,9 @@
 '''given a binary tree with node data 0 or 1 find the sum of all binary
 numbers formed by combining root-to-node data with root data as MSB
 '''
+
+
+def get_sum_root_to_leaf(n):
+    pass
+
+
