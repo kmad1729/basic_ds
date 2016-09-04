ce74f435d6698345290c325488235a6e4bd80a57
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-09-03 22:32:46 -0700

    adding unittest lines

--- epi/ch10/prb5_btree_path_leaf_sum.py
+++ epi/ch10/prb5_btree_path_leaf_sum.py
@@ -2,8 +2,12 @@
 numbers formed by combining root-to-node data with root data as MSB
 '''
 
+import unittest
+
 
 def get_sum_root_to_leaf(n):
     pass
 
 
+if __name__ == '__main__':
+    unittest.main()
