80ba5d5959c85b90c6936f643dfabbc463344361
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-09-09 10:07:04 -0700

    adding problem statement for prb 10

---
+++ epi/ch10/prb10_compute_in_order_successor.py
@@ -0,0 +1,5 @@
+''' Design an algorithm that computes the successor of a node in a binary tree.
+Assume that each node stores its parent.
+
+Hint: Study the node's right subtree. What if the node does not have a right subtree?
+'''
