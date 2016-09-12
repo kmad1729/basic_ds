9401672d8e96524ff5501e25e4f79c9d0dad753e
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-09-11 21:56:33 -0700

    adding solution to prb6

---
+++ epi/ch10/prb6_root_to_leaf_path_tgt_sum.py
@@ -0,0 +1,7 @@
+'''Write a program which takes as input an integer and a binary tree with
+integer weights, and checks if there exists a leaf whose path weight
+equals the given integer.
+
+path-weight: pw of a node is the sum of the integers on the unique path from the
+root to that node
+'''
