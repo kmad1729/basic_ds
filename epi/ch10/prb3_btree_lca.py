b39e0604204b3e0c08cb1254afa62c92b0a3c43a
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-09-13 09:26:23 -0700

    moved prb3 tests to the problem file

--- epi/ch10/prb3_btree_lca.py
+++ epi/ch10/prb3_btree_lca.py
@@ -1,6 +1,7 @@
 'get the least common ancestor of two nodes in a generic binary tree'
 
-from binary_tree import BinaryTree
+import unittest
+from binary_tree import BinaryTree, get_toy_binary_tree, get_toy_binary_tree2
 
 '''Binary tree represented as
 {'data': 1, 'left': {'data':-1, 'left': None, 'right': None}, 'right':None}
@@ -42,3 +43,33 @@ def _util_lca(subtree_root, n0, n1):
 
 def get_LCA(tr, n0, n1):
     return _util_lca(tr.root, n0, n1)['LCA_node']
+
+class TestLCA(unittest.TestCase):
+    def test_lca(self):
+        tr = get_toy_binary_tree2()
+        n0 = tr.root['left']['left'] #9
+        n1 = tr.root['left']['right']['left'] #8
+
+        lca_node = get_LCA(tr, n0, n1)
+        self.assertEqual(lca_node['data'], 3)
+
+    def test_lca2(self):
+        tr = get_toy_binary_tree2()
+        n0 = tr.root['right']['left']['left']['right'] #23
+        n1 = tr.root['right']['right']['right'] #86
+
+        lca_node = get_LCA(tr, n0, n1)
+        self.assertEqual(lca_node['data'], 8)
+
+    def test_lca_equal_nodes(self):
+        tr = get_toy_binary_tree2()
+        n0 = tr.root['right']['left']['left']['right'] #23
+        n1 = tr.root['right']['left']['left']['right'] #23
+
+        lca_node = get_LCA(tr, n0, n1)
+        self.assertEqual(lca_node['data'], 23)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
