429f0ed425d9716bd5ad2b2f8c59afa135dfaa62
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-09-25 11:45:31 -0700

    done with small part of reduction. still need to fix nodes that dont belong to rand_edge

--- coursera_algo/week3/find_min_cut.py
+++ coursera_algo/week3/find_min_cut.py
@@ -15,8 +15,25 @@ def reduce_graph(G, new_node_id):
     Reassign edges and compute the new edges after reduction
     '''
 
-    rand_edge = get_random_edge(G)
+    rand_src, rand_dest = get_random_edge(G)
+    print (rand_src, rand_dest)
+
+    src_adjacent_nodes = G[rand_src]
+    dest_adjacent_nodes = G[rand_dest]
+
+    del G[rand_src]
+    del G[rand_dest]
+
+    #reduction step
+    G[new_node_id] = []
+    for n in src_adjacent_nodes:
+        if n not in [rand_src, rand_dest]:
+            G[new_node_id].append(n)
+
+    for n in dest_adjacent_nodes:
+        if n not in [rand_src, rand_dest] and n not in G[new_node_id]:
+            G[new_node_id].append(n)
 
 
-    print rand_edge
+    print(G)
 
