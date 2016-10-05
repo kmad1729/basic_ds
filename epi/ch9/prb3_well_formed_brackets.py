685be2c699623a48cb6ce1d355180612ec5adbe7
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-10-05 08:50:30 -0700

    adding prblem statement for well formed brackets problem

---
+++ epi/ch9/prb3_well_formed_brackets.py
@@ -0,0 +1,9 @@
+'''A string over the characters "{,},(,),[,]" is said to be well-formed if the
+different types of brackets match in the CORRECT ORDER.
+W.A.P that tests if a string is well-formed
+E.g. 
+(()) <- well-formed
+([{}{}{}])() <- well-formed
+({ <- not well-formed
+[([[)]]] <- not well-formed
+'''
