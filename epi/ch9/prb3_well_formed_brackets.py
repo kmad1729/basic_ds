c51e970849490c0ed81355397db24dd6794e0e15
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-10-05 22:04:14 -0700

    adding well formed solution

--- epi/ch9/prb3_well_formed_brackets.py
+++ epi/ch9/prb3_well_formed_brackets.py
@@ -7,3 +7,43 @@ E.g.
 ({ <- not well-formed
 [([[)]]] <- not well-formed
 '''
+
+import unittest
+
+def is_well_formed(inp_str):
+    util_stack = []
+    for c in iter(inp_str):
+        if c in ['}', ')', ']']:
+            if len(util_stack) == 0:
+                return False
+            if c == '}' and util_stack[-1] != '{':
+                return False
+            elif c == ')' and util_stack[-1] != '(':
+                return False
+            elif c == ']' and util_stack[-1] != '[':
+                return False
+            else:
+                util_stack.pop()
+        else:
+            util_stack.append(c)
+
+    return len(util_stack) == 0
+
+class Test_well_formedness(unittest.TestCase):
+    def test_basic(self):
+        test_cases = {
+                '(())': True,
+                '([{}{}{}])()': True,
+                '({' : False,
+                '[([[)]]]' : False,
+                }
+
+        for inp_str, expected in test_cases.items():
+            self.assertEquals(expected, is_well_formed(inp_str),
+                    'expected {e} did not match function output "{s}"'.format(
+                        e=expected, s=inp_str))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
