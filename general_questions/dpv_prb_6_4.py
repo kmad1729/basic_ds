238ca5f399e4610896ba27190d07ab0ebd935637
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-10-23 08:38:54 -0700

    adding a working program to compute if a valid sequence of words can be created from corrupted text

--- general_questions/dpv_prb_6_4.py
+++ general_questions/dpv_prb_6_4.py
@@ -16,8 +16,23 @@ Eg:
 import unittest
 
 
+def is_valid_word(s):
+    words = {"i", "it", "was", "the", "best", "of", "times",
+            "bed", "bath","and","beyond", "be"}
+    return s in words
+
+
 def is_s_valid_word_seq(s):
-    return True
+    l_s = list(s)
+    util_arr = [False] * (len(l_s) + 1)
+    util_arr[0] = True
+    for i in range(len(l_s) + 1):
+        for j in range(i+1):
+            if util_arr[j-1]:
+                if is_valid_word(''.join(s[j-1:i])):
+                    util_arr[i] = True
+                    break
+    return util_arr[-1]
 
 
 class Test_is_s_valid_seq(unittest.TestCase):
@@ -30,9 +45,14 @@ class Test_is_s_valid_seq(unittest.TestCase):
                 "itwa" : False,
                 "itwas" : True,
                 "itwasthebestoftimes" : True,
+                "iti":True,
+                "wasbesti":True,
+                "bedbathandbeyond":True,
+                "bedbathandbe":True,
                 }
         for i in test_cases:
-            self.assertEquals(is_s_valid_word_seq(i), test_cases[i])
+            self.assertEqual(is_s_valid_word_seq(i), test_cases[i], 
+                    "({s}) expected to be valid? {e}".format(s=i, e=test_cases[i]))
 
 
 if __name__ == '__main__':
