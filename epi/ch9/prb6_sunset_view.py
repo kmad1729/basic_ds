fcb47cf94f53d8ca1bd18f8161a1e81908946685
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-10-27 23:17:50 -0700

    adding problem statement for buildings with sunset view

--- epi/ch9/prb6_sunset_view.py
+++ epi/ch9/prb6_sunset_view.py
@@ -1,3 +1,8 @@
-'sunset view problem compute sunsed view"
+''' We are given height of buildings from west to east.
+These buildings have windows facing west.
+Return indices of building with sunset view.
+E.g [6, 3, 7, 2, 1, 8, 9, 4]
+    [0,    2,       5, 6,  ] <<< Buildings with sunset view
+'''
 
 
